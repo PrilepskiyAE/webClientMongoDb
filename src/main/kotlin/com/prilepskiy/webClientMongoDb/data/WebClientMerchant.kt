750abf4911623dabdbe362e4e90f1e1a441d3420
Prilepskiy_ae <alexey.prilepskiy@btb.dev>
2023-07-11 01:13:47 +0700

    web add

---
+++ src/main/kotlin/com/prilepskiy/webClientMongoDb/data/WebClientMerchant.kt
@@ -0,0 +1,36 @@
+package com.prilepskiy.webClientMongoDb.data
+
+import jakarta.annotation.PostConstruct
+
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToFlux
+
+@Component
+class WebClientMerchant {
+    private val client = WebClient.create("http://192.168.0.121:8080/merchants")
+
+//    @PostConstruct
+//    private  fun getDate(){
+//
+//        client.get()
+//            .retrieve()
+//            .bodyToFlux<String>()//тут будет MerchantOffer
+//            .delayElements(Duration.ofMillis(1200))
+//            .doOnNext {
+//                println(it)
+//            }.
+//
+//
+//
+//    }
+@PostConstruct
+private  fun getDate(){
+    client.get()
+        .retrieve()
+        .bodyToFlux<String>()//тут будет MerchantOffer
+        .toStream().forEach {
+            println(it) //тут будет запись в бд
+        }
+}
+}
